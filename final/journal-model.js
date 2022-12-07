4f9b4f228020a99c0322654df479bc61a59a0f3c
HonorThe1nsane <troymd2009@hotmail.com>
2022-12-06 22:00:40 -0700

    initial commit for final project

---
+++ final/journal-model.js
@@ -0,0 +1,11 @@
+function getDate() {
+    let displayDate = document.getElementById("date");
+
+    let date = displayDate.value;
+
+    return date;
+    // var date = Date.now();
+    // let displayDate= date.toString().split(' ').slice(0, 4).join(' ');
+
+    // return displayDate;
+}
