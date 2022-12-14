ad8f3293c4ce8da25b8a8f26e15938532319728d
Troy Davidson <troymd2009@hotmail.com>
2022-12-13 22:55:20 -0700

    fixed issue that would not allow the todo item to close

--- toDoApp/todo.js
+++ toDoApp/todo.js
@@ -1,61 +1,64 @@
 // Create a "close" button and append it to each list item
 
 class ToDoList {
+    
 
 
-    hideClosedItem() {
-        var list = document.querySelector('ul');
-        list.addEventListener('click', function (ev) {
-            if (ev.target.tagName === 'LI') {
-                ev.target.classList.toggle('checked');
-            }
-        }, false);
-    }
-    addCheck() {
-        var list = document.querySelector('ul');
-        list.addEventListener('click', function (ev) {
-            if (ev.target.tagName === 'LI') {
-                ev.target.classList.toggle('checked');
-            }
-        }, false);
-    }
-    getToDo() {
-        //close button
-        var myNodelist = document.getElementsByTagName("LI");
-        var j;
-        for (j = 0; j < myNodelist.length; j++) {
-            var span = document.createElement("SPAN");
-            var txt = document.createTextNode("\u00D7");
-            span.className = "close";
-            span.appendChild(txt);
-            myNodelist[j].appendChild(span);
+hideClosedItem() {
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
         }
-
-        var li = document.createElement("li");
-        var inputValue = document.getElementById("myInput").value;
-        var t = document.createTextNode(inputValue);
-        li.appendChild(t);
-        if (inputValue === '') {
-            alert("You must enter something!");
-        } else {
-            document.getElementById("myUL").appendChild(li);
+    }, false);
+}
+addCheck() {
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function (ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
         }
-        document.getElementById("myInput").value = "";
-
-
+    }, false);
+}
+getToDo() {
+    //close button
+    var myNodelist = document.getElementsByTagName("LI");
+    var j;
+    for (j = 0; j < myNodelist.length; j++) {
         var span = document.createElement("SPAN");
         var txt = document.createTextNode("\u00D7");
         span.className = "close";
         span.appendChild(txt);
-        li.appendChild(span);
-        let i = 0;
-        for (i = 0; i < close.length; i++) {
-            close[i].onclick = function () {
-                var div = this.parentElement;
-                div.style.display = "none";
-            }
-        }
+        myNodelist[j].appendChild(span);
     }
+
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must enter something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    var close = document.getElementsByClassName("close");
+    var i;
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+            }
+    }
+}
 
 }
 
